ce2e6f04310b3a31c39e23fdda71eaec4011ea1a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-08-28 12:02:59 -0400

    Add files via upload

---
+++ sl.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+import os,platform,socket,sys,time;
+import views;
+import subprocess as sub;
+#Prefix System
+cmd_prefix="/";
+shell_type="unknown";
+user_agent_stub="SL/OS";
+if platform.system()=='Windows':
+    cmd_prefix="> ";
+    shell_type="WinCMD";
+    user_agent_stub="SL/Windows";
+elif platform.system()=='Linux':
+    cmd_prefix="$ ";
+    shell_type="Unix-like";
+    user_agent_stub="SL/Linux";
+##endif
+#Serverlink Client
+class SL_Client:
+    def __init__(self):
+        self.isConnecting=False;
+        self.isConnected=False;
+        self.isInitialized=False;
+        self.isCLIInitialized=False;
+        self.currentUrl="";
+        self.sshConnection=None;
+        self.isSSHConnected=False;
+        self.curr_path="";
+        self.curr_user="";
+        self.creds="";
+        self.sl_user_agent="SL-Client/1.0 Mozilla/5.0 ("+user_agent_stub+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+        self.commands={
+            "connect": {
+                "desc": "initiates a SSH connection to provided hostname",
+                "valid": ["connect"],
+                "arguments": "[hostname] [port (opt)] <auth (-cred)(opt)>"
+            },
+            "config": {
+                "desc": "open the configuration menu",
+                "valid": ["cfg","config","configuration"],
+                "arguments": "None"
+            },
+            "cinfo": {
+                "desc": "displays information about the current connection",
+                "valid": ["cinfo"],
+                "arguments": "None"
+            },
+            "info": {
+                "desc": "displays information about the client",
+                "valid": ["info"],
+                "arguments": "None"
+            },
+            "exit": {
+                "desc": "exits the CLI",
+                "valid": ["exit","quit","leave"],
+                "arguments": "None"
+            },
+        };
+        self.info={
+            "version":"3.48",
+            "sshVer":"OpenSSH-2",
+            "shell":shell_type,
+        };
+        self.ip="";
+        self.port=0;
+    ##end
+    def get_connection_info(self):
+        if not self.isConnected:
+            self.print_err("Not connected to a server, no connection info available.");
+        else:
+            conn_info={
+                "ip":self.ip,
+                "port":str(self.port),
+                "url":self.currentUrl,
+                "current_path":self.curr_path,
+                "user":self.curr_user,
+                "user-agent":f"'{self.sl_user_agent}'"
+            };
+            for key,value in conn_info.items():
+                print(f"  {key}:{value}");
+            ##end
+        ##endif
+    ##end
+    def connect(self,host,port=22,credentials=""):
+        global usr,hostname;
+        usr="";
+        hostname=host;
+        if len(host.split('@'))>1:
+            usr=host.split('@')[0];
+            hostname=host.split('@')[1];
+        ##endif
+        if self.isConnected:
+            self.print_err("Already connected to a server");
+            return;
+        ##endif
+        if not self.isInitialized:
+            self.print_err("Serverlink not initialized");
+            return;
+        ##endif
+        if not self.isConnecting:
+            self.isConnecting=True;
+            self.curr_user=usr;
+            self.creds=credentials;
+            self.print_info(f"Connecting to https://{hostname} on port {port}. Please wait...");
+            time.sleep(1);
+            if self.hostname_resolves(hostname):
+                self.print_info(f"Connected to https://{hostname}.");
+                self.isConnecting=False;
+                self.isConnected=True;
+                self.ip=hostname;
+                self.port=port;
+                self.currentUrl=f"https://{hostname}:{port}";
+                self.initialize_cli(hostname);
+            else:
+                self.print_err(f"Failed to connect to https://{hostname}.\nRequest failed or Hostname does not resolve.");
+                self.isConnecting=False;
+            ##endif
+        else:
+            self.print_err("Already connecting to a server");
+        ##endif
+    ##end
+    def initialize_ssh(self,hostname,port,usr):
+        cmd=f"ssh {usr}@{hostname}" if usr!="" else f"ssh {hostname}";
+        try:
+            self.sshConnection=sub.Popen(cmd,shell=True,stdin=sub.PIPE,stdout=subprocess.PIPE, stderr=sub.PIPE);
+        except Exception as e:
+            self.print_err("Failed to initialize SSH connection");
+            return 1;
+        ##endtry
+        return 0;
+    ##end
+    def ssh_communicate(self,command):
+        if not self.isConnected:
+            self.print_err("Not connected to a server");
+        elif self.isSSHConnected:
+            self.sshConnection.stdin.write(command.encode());
+            self.sshConnection.stdin.flush();
+        ##endif
+    ##end
+    def ssh_close(self):
+        if not self.isConnected:
+            self.print_err("Not connected to a server");
+        elif self.isSSHConnected:
+            self.isSSHConnected=False;
+            self.sshConnection.terminate();
+            self.print_info("SSH connection closed");
+        ##endif
+    ##end
+    def initialize_cli(self,hostname):
+        if not self.isCLIInitialized:
+            self.print_info("Serverlink CLI initializing. Please wait...")
+            self.isCLIInitialized=True;
+            time.sleep(0.1);
+            self.print_info("<SHELL STARTING>");
+            self.initialize_ssh(hostname,self.port,self.curr_user);
+            time.sleep(0.9);
+            while self.isConnected and self.sshConnection and self.sshConnection.poll() is None:
+                output=self.sshConnection.stdout;
+                for line in output.splitlines():
+                    print(line);
+                ##end
+                cmd=input(cmd_prefix);
+                if cmd=='exit' or cmd=='quit' or cmd=='q':
+                    self.ssh_close();
+                    self.isConnected=False;
+                ##endif
+            ##end
+        ##endif
+    ##end
+    def disconnect(self):
+        if self.isConnected:
+            self.print_info(f"Saving and disconnecting from https://{self.ip}.");
+            time.sleep(1);
+            self.print_info(f"Disconnected from https://{self.ip}.");
+            self.isConnected=False;
+        ##endif
+    ##end
+    def begin_shell(self,hostname,port,credentials):
+        return 0;
+    ##ebd
+    def get_command(self,cmd):
+        result=""
+        for cmd_name,cmd_info in self.commands.items():
+            if cmd in cmd_info["valid"]:
+                result=cmd_name;
+                break;
+            ##endif
+        ##end
+        return result;
+    ##end
+    def initialize(self):
+        if not self.isInitialized:
+            self.print_info("Serverlink initializing.");
+            self.isInitialized=True;
+            self.isConnected=False;
+            self.isConnecting=False;
+            self.currentUrl="";
+        else:
+            self.print_err("Serverlink already initialized.");
+        ##endif
+    ##end
+    def hostname_resolves(self,hostname):
+        try:
+            socket.gethostbyname(hostname);
+            return True;
+        except socket.error:
+            self.print_err("Unable to get address info");
+            return False;
+        ##endif
+    ##end
+    def run_cmd(self,cmd,args):
+        if cmd=="connect":
+            if len(args)<1:
+                self.print_err("No hostname provided, process exited.");
+            elif len(args)<2:
+                self.connect(args[0], 22, "");
+            elif len(args)<3:
+                self.connect(args[0], int(args[1]), "");
+            elif len(args)==3:
+                if args[2].startswith("-cred="):
+                    self.connect(args[0], int(args[1]), args[2][6:]);
+                else:
+                    self.print_err("Invalid credentials provided, process exited.");
+                ##endif
+            else:
+                self.print_err("Too many arguments provided, process exited.");
+            ##endif
+        elif cmd=="info":
+            if len(args)<1:
+                for key,value in self.info.items():
+                    print(f"  {key}: {value}");
+                ##end
+            ##endif
+        elif cmd=="cinfo":
+            if len(args)<1:
+                self.get_connection_info();
+            ##endif
+        elif cmd=="config":
+            views.configMenu();
+        ##endif
+    ##end
+    def print_info(self,output):
+        print(f"SL: {output}");
+    ##end
+    def print_err(self,output):
+        print(f"SL Error: {output}");
+    ##end
+##end
+
+def main(argc:int,argv:list[str]):
+    slc=SL_Client();
+    slc.initialize();
+    magic_exit_code=False;
+    if argc < 2:
+        print("Welcome to Serverlink, what would you like to do?");
+        print("Type 'help' for a list of commands");
+        while not magic_exit_code:
+            input_str=input(f"{cmd_prefix}");
+            args=input_str.split(' ');
+            if input_str == "":
+                continue;
+            elif args[0]=="help":
+                print("Commands:");
+                for cmd,details in slc.commands.items():
+                    print(f"  {cmd} - {details['desc']}");
+                    print(f"  Arguments: {details['arguments']}");
+                ##end
+            elif slc.get_command(args[0])!="":
+                cmdname = slc.get_command(args[0]);
+                if cmdname=="exit":
+                    magic_exit_code=True;
+                else:
+                    args.pop(0)
+                    slc.run_cmd(cmdname,args);
+                ##endif
+            else:
+                print("Invalid command.");
+            ##endif
+        ##end
+    elif argc > 1:
+        args=argv[1:];
+        cmd=args[0];
+        if cmd=="help":
+            print("Commands:")
+            for cmd,details in slc.commands.items():
+                print(f"  {cmd} - {details['desc']}");
+                print(f"  Arguments: {details['arguments']}");
+            ##end
+        elif slc.get_command(cmd)!="":
+            cmdname=slc.get_command(cmd);
+            args.pop(0);
+            slc.run_cmd(cmdname,args);
+        else:
+            print("Invalid command.");
+        ##endif
+    ##endif
+##end
+main(len(sys.argv),sys.argv);
---
+++ views.py
@@ -0,0 +1,247 @@
+import json,os,platform,re,socket,sys,threading,time;
+import tkinter as tk;
+from tkinter import ttk;
+#HELPER FUNCTIONS
+def dirExists(path):
+    return os.path.exists(path);
+##end
+def createWinADFIfNotPresent():
+    appDataPath=os.getenv('APPDATA');
+    if (appDataPath!=None):
+        if (not dirExists(f'{appDataPath}/Serverlink')):
+            os.mkdir(f'{appDataPath}/Serverlink');
+        ##endif
+    else:
+        print('Failed to find %APPDATA% directory..');
+    ##endif
+##end
+def createLinuxADFIfNotPresent():
+    if (not dirExists('~/.local/share/Serverlink')):
+        os.mkdir('~/.local/share/Serverlink');
+    ##endif
+##end
+def createCFGFolderIfNotPresent():
+    os_name=platform.system();
+    if (os_name=='Linux'):
+        createLinuxADFIfNotPresent();
+        if (dirExists('~/.local/share/Serverlink/cfg')):
+           return '~/.local/share/Serverlink/cfg';
+        else:
+            os.mkdir('~/.local/share/Serverlink/cfg');
+            return '~/.local/share/Serverlink/cfg';
+        ##endif
+    elif (os_name=='Windows'):
+        appDataPath=os.getenv('APPDATA');
+        createWinADFIfNotPresent();
+        if (appDataPath!=None):
+            if (dirExists(f'{appDataPath}/Serverlink/cfg')):
+                return f'{appDataPath}/Serverlink/cfg';
+            else:
+                os.mkdir(f'{appDataPath}/Serverlink/cfg');
+                return f'{appDataPath}/Serverlink/cfg';
+            ##endif
+        else:
+            print('Failed to find %APPDATA% directory..');
+        ##endif
+    else:
+        print('Unsupported OS, sorry.');
+    ##endif
+##end
+
+#VIEWS FUNCTIONS
+def configMenu():
+    global configFile,cfgdat;
+    cfgdat={
+        "isDarkMode":False,
+        "fg":"default",
+        "knownHosts":{},
+    };
+    cfgPath=createCFGFolderIfNotPresent();
+    if (dirExists(f'{cfgPath}/main.cfg')):
+        with open(f'{cfgPath}/main.cfg','r') as configFile:
+            cfgdat=json.load(configFile);
+            configFile.close();
+        ##endwith
+    else:
+        with open(f'{cfgPath}/main.cfg','w+') as configFile:
+            configFile.write(json.dumps(cfgdat));
+            configFile.close();
+        ##endwith
+    ##endif
+    win=tk.Tk();
+    win.geometry('640x480');
+    win.title('Serverlink - Settings');
+    win_h=win.winfo_height();
+    win_w=win.winfo_width();
+    styles=ttk.Style();
+    if (cfgdat['fg']!="default"):
+        fgColor=cfgdat['fg'];
+    ##endif
+    styles.theme_create('Light',parent="default",settings={
+        'TFrame':{
+            'configure':{
+                "background":"#fff",
+            },
+        },
+        'Main.TFrame':{
+            'configure':{
+                "background":"#000",
+            },
+        },
+        'SHS.TFrame':{
+            'configure':{
+                'background':"#fff",
+            },
+        },
+        'KH.TFrame':{
+            'configure':{
+                'background':"#fff",
+            },
+        },
+        'TNotebook':{
+            'configure':{
+                'background':"#eee",
+            },
+        },
+        'TNotebook.Tab':{
+            'configure':{
+                'background':"#fff",
+                'foreground':"#000",
+                'lightcolor':"#fff",
+                'padding':5,
+            },
+            "map": {
+                "background": [("active", "#fff"), ("selected", "#fff"), ("!selected", "#eee")],
+            },
+        },
+        'TCheckbutton':{
+            'configure':{
+                'background':"#eee",
+                'foreground':"#000",
+                'indicatorbackground':"#eee",
+                'indicatorforeground':"#000",
+                'padding':5,
+            },
+        },
+    });
+    styles.theme_create('Dark',parent="default",settings={
+        'TFrame':{
+            'configure':{
+                "background":"#fff",
+            },
+        },
+        'Main.TFrame':{
+            'configure':{
+                "background":"#000",
+            },
+        },
+        'SHS.TFrame':{
+            'configure':{
+                'background':"#333",
+            },
+        },
+        'KH.TFrame':{
+            'configure':{
+                'background':"#333",
+            },
+        },
+        'TNotebook':{
+            'configure':{
+                'background':"#000",
+            },
+        },
+        'TNotebook.Tab':{
+            'configure':{
+                'foreground':"#fff",
+                'lightcolor':"#333",
+                'padding':5,
+            },
+            "map": {
+                "background": [("active", "#333"), ("selected", "#333"), ("!selected", "#000")],
+            },
+        },
+        'TCheckbutton':{
+            'configure':{
+                'background':"#333",
+                'foreground':"#fa0",
+                'indicatorbackground':"#fff",
+                'indicatorforeground':"#0f0",
+                'padding':5,
+            },
+        },
+    });
+    styles.theme_create('Default',parent="default",settings={
+        'TFrame':{
+            'configure':{
+                "background":"#fff",
+            },
+        },
+        'Main.TFrame':{
+            'configure':{
+                "background":"#000",
+            },
+        },
+        'SHS.TFrame':{
+            'configure':{
+                'background':"#fff",
+            },
+        },
+        'KH.TFrame':{
+            'configure':{
+                'background':"#fff",
+            },
+        },
+        'TNotebook':{
+            'configure':{
+                'background':"#eee",
+            },
+        },
+        'TNotebook.Tab':{
+            'configure':{
+                'background':"#000",
+                'lightcolor':"#fff",
+                'padding':5,
+            },
+            "map": {
+                "background": [("active", "#fff"), ("selected", "#fff"), ("!selected", "#eee")],
+            },
+        },
+        'TCheckbutton':{
+            'configure':{
+                'background':"#fff",
+                'foreground':"#000",
+                'indicatorbackground':"#eee",
+                'indicatorforeground':"#000",
+                'padding':5,
+            },
+        },
+    });
+    root=ttk.Frame(win,style='Main.TFrame');
+    root.pack(expand=1,fill=tk.BOTH);
+    notebook=ttk.Notebook(root);
+    tab1=ttk.Frame(notebook,style="SHS.TFrame");
+    tab2=ttk.Frame(notebook,style="KH.TFrame");
+    notebook.add(tab1,text='Shell Settings');
+    notebook.add(tab2,text='Known Hosts');
+    var1=tk.IntVar();
+    def setDarkMode(c1):
+        if (var1.get()==1):
+            cfgdat['isDarkMode']=True;
+            styles.theme_use('Dark');
+        else:
+            cfgdat['isDarkMode']=False;
+            styles.theme_use('Light');
+        ##endif
+    ##end
+    def bindFn(fn,*args,**kwargs):
+        def nfn(*args2,**kwargs2):
+            fn(*args,*args2,**kwargs,**kwargs2);
+        ##end
+        return nfn;
+    ##end
+    c1=tk.Checkbutton(tab1,text="Dark Mode",variable=var1,onvalue=1,offvalue=0);
+    c1['command']=bindFn(setDarkMode,c1);
+    c1.pack(expand=1,fill=tk.X,anchor="n");
+    notebook.pack(expand=1,fill=tk.BOTH);
+    win.mainloop();
+##end
