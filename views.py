2ab988e1b15f0c39078f2c7004ef5a58655c595a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-10-10 14:16:53 -0400

    update sl and views

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 import json,os,platform,re,socket,sys,threading,time;
 import tkinter as tk;
-from tkinter import FALSE, ttk;
+from tkinter import ttk;
 #HELPER FUNCTIONS
 def dirExists(path):
     return os.path.exists(path);
@@ -17,7 +17,7 @@ def createWinADFIfNotPresent():
 ##end
 def createLinuxADFIfNotPresent():
     home_directory=os.path.expanduser('~');
-    directory_path=os.path.join(home_directory,'Serverlink');
+    directory_path=os.path.join(home_directory,'.serverlink');
     if (not dirExists(directory_path)):
         os.mkdir(directory_path);
     ##endif
@@ -26,7 +26,7 @@ def createCFGFolderIfNotPresent():
     os_name=platform.system();
     if (os_name=='Linux'):
         home_directory=os.path.expanduser('~');
-        directory_path=os.path.join(home_directory,'Serverlink');
+        directory_path=os.path.join(home_directory,'.serverlink');
         createLinuxADFIfNotPresent();
         conf_path=os.path.join(directory_path,'conf');
         if (dirExists(conf_path)):
@@ -74,7 +74,6 @@ class SL_GUI:
         if (not self.window_exists):
             self.make_window();
         ##endif
-        self.print_info("Loading page...");
         self.load_page(page);
     ##end
     def make_window(self):
@@ -87,14 +86,77 @@ class SL_GUI:
         self.root.geometry('640x480');
         self.root.title('Serverlink');
     ##end
+    def loadframe(self):
+        pass;
+    ##end
     def load_page(self,page):
+        self.print_info("Loading page...");
+        self.loadframe();
         if page=="conf":
             self.configMenu();
+            self.window_exists=False;
         elif page=="index":
             self.root.title("Serverlink - Welcome");
             self.root.mainloop();
             self.window_exists=False;
         ##endif
+    ##end
+    class Procedural_Spinner:
+        def __init__(self,root,canvas_width=128,canvas_height=128,duration=1,num_frames=100,width=2,color="black"):
+            self.canvas_width=canvas_width;
+            self.canvas_height=canvas_height;
+            self.duration=duration;
+            self.num_frames=num_frames;
+            self.arc_start=90;
+            self.arc_offset=0;
+            self.arc_width=width;
+            self.curr_extent=0;
+            self.decreasing=False;
+            self.stopped=False;
+            self.spinner_color=color;
+            self.frame=0;
+            self.canvas=tk.Canvas(root,width=self.canvas_width,height=self.canvas_height)
+            self.canvas.pack();
+            self.update_arc();
+        ##end
+        def update_arc(self):
+            arc_step=360//self.num_frames;
+            self.arc_offset=((self.frame*arc_step)+self.arc_start)*2.5;
+            self.arc_offset%=360;  # keep the angle between 0 and 359
+            self.canvas.delete("arc");  # delete the previous arc
+
+            # Toggle the direction of the arc extent change
+            if self.curr_extent>=270 and not self.decreasing:
+                self.decreasing=True;
+                self.arc_start+=90;
+                if self.arc_start>=360:
+                    self.arc_start=0;
+                ##endif
+            elif self.curr_extent<=0 and self.decreasing:
+                self.decreasing=False;
+                self.arc_start+=90;
+                if self.arc_start>=360:
+                    self.arc_start=0;
+                ##endif
+            ##end
+            # Update the extent of the arc
+            if self.decreasing:
+                self.curr_extent-=arc_step;
+            else:
+                self.curr_extent+=arc_step;
+            ##endif
+            # Ensure the extent stays within the bounds
+            self.curr_extent=max(0,min(self.curr_extent,270));
+            arc_padding=20;
+            bbox=(arc_padding,arc_padding,self.canvas_width-arc_padding,self.canvas_height-arc_padding);
+            # Create a new arc
+            self.canvas.create_arc(bbox,start=self.arc_offset,extent=self.curr_extent,width=self.arc_width,style="arc",tags="arc",stroke=self.spinner_color);
+            # Schedule the next update
+            self.frame+=1;
+            if not self.stopped:
+                self.canvas.after((self.duration*1000)//self.num_frames,self.update_arc);
+            ##endif
+        ##end
     ##end
     def configMenu(self):
         global configFile,cfgdat;
@@ -104,17 +166,23 @@ class SL_GUI:
             "knownHosts":{},
         };
         cfgPath=createCFGFolderIfNotPresent();
-        if (dirExists(f'{cfgPath}/main.cfg')):
-            with open(f'{cfgPath}/main.cfg','r') as configFile:
+        def write_to_config(data):
+            with open(f'{cfgPath}/main.json','w+') as configFile:
+                configFile.write(json.dumps(data));
+                configFile.close();
+            ##endwith
+        ##end
+        if (dirExists(f'{cfgPath}/main.json')):
+            with open(f'{cfgPath}/main.json','r') as configFile:
                 cfgdat=json.load(configFile);
                 configFile.close();
             ##endwith
         else:
-            with open(f'{cfgPath}/main.cfg','w+') as configFile:
-                configFile.write(json.dumps(cfgdat));
-                configFile.close();
-            ##endwith
+            write_to_config(cfgdat);
         ##endif
+        if not self.window_exists:
+            self.make_window();
+        ##end
         win=self.root;
         win.title('Serverlink - Settings');
         win_h=win.winfo_height();
@@ -218,52 +286,6 @@ class SL_GUI:
                 },
             },
         });
-        styles.theme_create('Default',parent="default",settings={
-            'TFrame':{
-                'configure':{
-                    "background":"#fff",
-                },
-            },
-            'Main.TFrame':{
-                'configure':{
-                    "background":"#000",
-                },
-            },
-            'SHS.TFrame':{
-                'configure':{
-                    'background':"#fff",
-                },
-            },
-            'KH.TFrame':{
-                'configure':{
-                    'background':"#fff",
-                },
-            },
-            'TNotebook':{
-                'configure':{
-                    'background':"#eee",
-                },
-            },
-            'TNotebook.Tab':{
-                'configure':{
-                    'background':"#000",
-                    'lightcolor':"#fff",
-                    'padding':5,
-                },
-                "map": {
-                    "background": [("active", "#fff"), ("selected", "#fff"), ("!selected", "#eee")],
-                },
-            },
-            'TCheckbutton':{
-                'configure':{
-                    'background':"#fff",
-                    'foreground':"#000",
-                    'indicatorbackground':"#eee",
-                    'indicatorforeground':"#000",
-                    'padding':5,
-                },
-            },
-        });
         root=ttk.Frame(win,style='Main.TFrame');
         root.pack(expand=1,fill=tk.BOTH);
         notebook=ttk.Notebook(root);
@@ -272,13 +294,22 @@ class SL_GUI:
         notebook.add(tab1,text='Shell Settings');
         notebook.add(tab2,text='Known Hosts');
         var1=tk.IntVar();
+        if cfgdat['isDarkMode']:
+            styles.theme_use('Dark');
+            var1.set(1);
+        else:
+            styles.theme_use('Light');
+            var1.set(0);
+        ##endif
         def setDarkMode(c1):
             if (var1.get()==1):
                 cfgdat['isDarkMode']=True;
                 styles.theme_use('Dark');
+                write_to_config(cfgdat);
             else:
                 cfgdat['isDarkMode']=False;
                 styles.theme_use('Light');
+                write_to_config(cfgdat);
             ##endif
         ##end
         def bindFn(fn,*args,**kwargs):
